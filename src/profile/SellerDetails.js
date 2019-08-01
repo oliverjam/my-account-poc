1d031a96b6108521d34fc62987fae71dabb9ccc5
oliver.phillips <oliver.phillips@ticketmaster.co.uk>
2019-08-01 17:27:12 +0100

    Prototype KYC UI

--- src/profile/SellerDetails.js
+++ src/profile/SellerDetails.js
@@ -1,8 +1,12 @@
 import React from "react";
 import Card from "../shared/Card";
 
-function SellerDetails() {
-  const [editing, setEditing] = React.useState(false);
+function SellerDetails({ location }) {
+  const searchParams = new URLSearchParams(location.search);
+  const kyc = searchParams.getAll("kyc");
+
+  const [editingInfo, setEditingInfo] = React.useState(false);
+  const [editingPayment, setEditingPayment] = React.useState(false);
   return (
     <div>
       <h1>Seller details</h1>
@@ -13,17 +17,59 @@ function SellerDetails() {
       </p>
       <Card>
         <Card.Title>Personal information</Card.Title>
-        <Card.Action onClick={() => setEditing(!editing)}>Edit</Card.Action>
+        <Card.Action onClick={() => setEditingInfo(!editingInfo)}>
+          <span aria-hidden="true" style={{ marginRight: 8 }}>
+            ✎
+          </span>
+          Edit
+        </Card.Action>
         <Card.Body>
+          <Kyc kyc={kyc} type="id">
+            Please upload proof of identity
+          </Kyc>
           <InputField
-            disabled={!editing}
+            disabled={!editingInfo}
             label="Date of birth"
             defaultValue="12/12/1996"
+            style={{ marginTop: kyc.includes("id") && 16 }}
           />
           <InputField
-            disabled={!editing}
+            disabled={!editingInfo}
             label="Phone number"
             defaultValue="+447789998888"
+            style={{ marginTop: editingInfo ? 16 : 8 }}
+          />
+        </Card.Body>
+      </Card>
+      <Card style={{ marginTop: 24 }}>
+        <Card.Title>Payment details</Card.Title>
+        <Card.Action onClick={() => setEditingPayment(!editingPayment)}>
+          <span aria-hidden="true" style={{ marginRight: 8 }}>
+            ✎
+          </span>
+          Edit
+        </Card.Action>
+        <Card.Body>
+          <Kyc kyc={kyc} type="bank">
+            Please upload a bank statement
+          </Kyc>
+          <InputField
+            disabled={!editingPayment}
+            label="Account holder name"
+            defaultValue="Sarah Thompson"
+            style={{ marginTop: kyc.includes("bank") && 16 }}
+          />
+          <InputField
+            disabled={!editingPayment}
+            label="Account number"
+            defaultValue="**** 1234"
+            style={{ marginTop: editingPayment ? 16 : 8 }}
+          />
+          <InputField
+            disabled={!editingPayment}
+            label="Sort code"
+            defaultValue="00-00-00"
+            style={{ marginTop: editingPayment ? 16 : 8 }}
           />
         </Card.Body>
       </Card>
@@ -31,12 +77,38 @@ function SellerDetails() {
   );
 }
 
-function InputField({ id, label, disabled, ...rest }) {
+function Kyc({ kyc, type, children, ...rest }) {
+  if (!kyc.includes(type)) return null;
   return (
-    <div style={{ display: disabled && "flex", marginTop: disabled ? 8 : 16 }}>
+    <details
+      style={{
+        boxShadow: "0 2px 4px hsla(220, 10%, 40%, 0.2)",
+        borderRadius: 4,
+        borderLeft: "4px solid red",
+        padding: 24
+      }}
+      {...rest}
+    >
+      <summary
+        style={{
+          fontWeight: 600
+        }}
+      >
+        <span>{children}</span>
+        {/* <span aria-hidden="true">〉</span> */}
+      </summary>
+      <InputField label="Document type" style={{ marginTop: 16 }} />
+      <input type="file" style={{ marginTop: 16 }} />
+    </details>
+  );
+}
+
+function InputField({ id, label, disabled, style, ...rest }) {
+  return (
+    <div style={{ display: disabled && "flex", ...style }}>
       <label
         htmlFor={id}
-        style={{ fontWeight: 500, fontSize: disabled ? 16 : 14 }}
+        style={{ fontWeight: 600, fontSize: disabled ? 16 : 14 }}
       >
         {label}
         {disabled && ": "}
@@ -59,7 +131,13 @@ const inputStyles = {
 };
 
 function Input({ disabled, ...rest }) {
-  return <input style={disabled ? disabledStyles : inputStyles} {...rest} />;
+  return (
+    <input
+      style={disabled ? disabledStyles : inputStyles}
+      disabled={disabled}
+      {...rest}
+    />
+  );
 }
 
 export default SellerDetails;
--- src/shared/Card.js
+++ src/shared/Card.js
@@ -1,13 +1,14 @@
 import React from "react";
 
-function Card({ as: Comp = "div", ...rest }) {
+function Card({ as: Comp = "div", style, ...rest }) {
   return (
     <Comp
       style={{
         position: "relative",
         borderRadius: 4,
         backgroundColor: "white",
-        boxShadow: "0 2px 6px hsla(220, 10%, 40%, 0.3"
+        boxShadow: "0 2px 6px hsla(220, 10%, 40%, 0.3",
+        ...style
       }}
       {...rest}
     />
