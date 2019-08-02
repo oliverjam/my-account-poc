c3c397c75d126ffa361c6b7cd5be35b59ede8470
oliver.phillips <oliver.phillips@ticketmaster.co.uk>
2019-08-02 13:03:51 +0100

    Split seller details cards into separate component

---
+++ src/profile/EditCard.js
@@ -0,0 +1,121 @@
+import React from "react";
+import Card from "../shared/Card";
+import Stack from "../shared/Stack";
+
+const EditContext = React.createContext();
+
+function useEditing() {
+  const editing = React.useContext(EditContext);
+  if (editing === undefined)
+    throw new Error("EditCard.Input must be rendered within an EditCard");
+  return editing;
+}
+
+function EditCard({ title, children, onEditing, disabled, ...rest }) {
+  const [editing, setEditing] = React.useState(false);
+
+  React.useEffect(() => {
+    if (onEditing) onEditing(editing);
+  }, [onEditing, editing]);
+
+  return (
+    <Card style={{ marginTop: 24 }} {...rest}>
+      <Card.Title>{title}</Card.Title>
+      {!editing && (
+        <Card.Action disabled={disabled} onClick={() => setEditing(true)}>
+          <span aria-hidden="true" style={{ marginRight: 8 }}>
+            ✎
+          </span>
+          Edit
+        </Card.Action>
+      )}
+      <Card.Body>
+        <EditContext.Provider value={editing}>{children}</EditContext.Provider>
+        {editing && (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-end"
+            }}
+          >
+            <button onClick={() => setEditing(false)}>Cancel</button>
+            <button
+              onClick={() => setEditing(false)}
+              style={{
+                marginLeft: 8,
+                background: "var(--primary)",
+                color: "white"
+              }}
+            >
+              Save changes
+            </button>
+          </div>
+        )}
+      </Card.Body>
+    </Card>
+  );
+}
+
+function Kyc({ kyc, type, children, ...rest }) {
+  const editing = useEditing();
+  if (!kyc.includes(type) || editing) return null;
+  return (
+    <details
+      style={{
+        boxShadow: "0 2px 4px hsla(220, 10%, 40%, 0.2)",
+        borderRadius: 4,
+        borderLeft: "4px solid red",
+        padding: 24
+      }}
+      {...rest}
+    >
+      <summary
+        style={{
+          fontWeight: 600
+        }}
+      >
+        <span>{children}</span>
+      </summary>
+      <Stack style={{ marginTop: 16 }}>
+        <Input label="Document type" />
+        <input type="file" />
+      </Stack>
+    </details>
+  );
+}
+
+function Input({ id, label, style, ...rest }) {
+  const editing = useEditing();
+  return (
+    <div style={style}>
+      <label htmlFor={id} style={{ fontSize: 14, fontWeight: 600 }}>
+        {label}
+      </label>
+      <input
+        id={id}
+        style={!editing ? disabledStyles : inputStyles}
+        disabled={!editing}
+        {...rest}
+      />
+    </div>
+  );
+}
+
+const disabledStyles = {
+  marginTop: 4,
+  border: "none",
+  background: "none"
+};
+
+const inputStyles = {
+  width: "100%",
+  marginTop: 4,
+  padding: 8,
+  border: "1px solid hsl(220, 10%, 90%)",
+  borderRadius: 4
+};
+
+EditCard.Kyc = Kyc;
+EditCard.Input = Input;
+
+export default EditCard;
--- src/profile/SellerDetails.js
+++ src/profile/SellerDetails.js
@@ -1,142 +1,53 @@
 import React from "react";
-import Card from "../shared/Card";
+import Stack from "../shared/Stack";
+import EditCard from "./EditCard";
 
 function SellerDetails({ location }) {
   const searchParams = new URLSearchParams(location.search);
   const kyc = searchParams.getAll("kyc");
 
-  const [editingInfo, setEditingInfo] = React.useState(false);
-  const [editingPayment, setEditingPayment] = React.useState(false);
+  const [someEditing, setSomeEditing] = React.useState(false);
   return (
     <div>
-      <h1>Seller details</h1>
-      <p>
-        On this page you can edit how you would like to get paid for any tickets
-        you sell on Ticketmaster. Any changes will affect any resale listings
-        you already have.
-      </p>
-      <Card>
-        <Card.Title>Personal information</Card.Title>
-        <Card.Action onClick={() => setEditingInfo(!editingInfo)}>
-          <span aria-hidden="true" style={{ marginRight: 8 }}>
-            ✎
-          </span>
-          Edit
-        </Card.Action>
-        <Card.Body>
-          <Kyc kyc={kyc} type="id">
-            Please upload proof of identity
-          </Kyc>
-          <InputField
-            disabled={!editingInfo}
-            label="Date of birth"
-            defaultValue="12/12/1996"
-            style={{ marginTop: kyc.includes("id") && 16 }}
-          />
-          <InputField
-            disabled={!editingInfo}
-            label="Phone number"
-            defaultValue="+447789998888"
-            style={{ marginTop: editingInfo ? 16 : 8 }}
-          />
-        </Card.Body>
-      </Card>
-      <Card style={{ marginTop: 24 }}>
-        <Card.Title>Payment details</Card.Title>
-        <Card.Action onClick={() => setEditingPayment(!editingPayment)}>
-          <span aria-hidden="true" style={{ marginRight: 8 }}>
-            ✎
-          </span>
-          Edit
-        </Card.Action>
-        <Card.Body>
-          <Kyc kyc={kyc} type="bank">
-            Please upload a bank statement
-          </Kyc>
-          <InputField
-            disabled={!editingPayment}
-            label="Account holder name"
-            defaultValue="Sarah Thompson"
-            style={{ marginTop: kyc.includes("bank") && 16 }}
-          />
-          <InputField
-            disabled={!editingPayment}
-            label="Account number"
-            defaultValue="**** 1234"
-            style={{ marginTop: editingPayment ? 16 : 8 }}
-          />
-          <InputField
-            disabled={!editingPayment}
-            label="Sort code"
-            defaultValue="00-00-00"
-            style={{ marginTop: editingPayment ? 16 : 8 }}
-          />
-        </Card.Body>
-      </Card>
+      <Stack space="1.5rem">
+        <h1>Seller details</h1>
+        <p>
+          On this page you can edit how you would like to get paid for any
+          tickets you sell on Ticketmaster. Any changes will affect any resale
+          listings you already have.
+        </p>
+        <EditCard
+          title="Personal information"
+          disabled={someEditing}
+          onEditing={editing => setSomeEditing(editing)}
+        >
+          <Stack>
+            <EditCard.Kyc kyc={kyc} type="id">
+              Please upload proof of identity
+            </EditCard.Kyc>
+            <EditCard.Input label="Date of birth" defaultValue="12/12/1996" />
+            <EditCard.Input label="Phone number" defaultValue="+447789998888" />
+          </Stack>
+        </EditCard>
+        <EditCard
+          title="Payment details"
+          disabled={someEditing}
+          onEditing={editing => setSomeEditing(editing)}
+        >
+          <Stack>
+            <EditCard.Kyc kyc={kyc} type="bank">
+              Please upload a bank statement
+            </EditCard.Kyc>
+            <EditCard.Input
+              label="Account holder name"
+              defaultValue="Sarah Thompson"
+            />
+            <EditCard.Input label="Account number" defaultValue="**** 1234" />
+            <EditCard.Input label="Sort code" defaultValue="00-00-00" />
+          </Stack>
+        </EditCard>
+      </Stack>
     </div>
-  );
-}
-
-function Kyc({ kyc, type, children, ...rest }) {
-  if (!kyc.includes(type)) return null;
-  return (
-    <details
-      style={{
-        boxShadow: "0 2px 4px hsla(220, 10%, 40%, 0.2)",
-        borderRadius: 4,
-        borderLeft: "4px solid red",
-        padding: 24
-      }}
-      {...rest}
-    >
-      <summary
-        style={{
-          fontWeight: 600
-        }}
-      >
-        <span>{children}</span>
-        {/* <span aria-hidden="true">〉</span> */}
-      </summary>
-      <InputField label="Document type" style={{ marginTop: 16 }} />
-      <input type="file" style={{ marginTop: 16 }} />
-    </details>
-  );
-}
-
-function InputField({ id, label, disabled, style, ...rest }) {
-  return (
-    <div style={{ display: disabled && "flex", ...style }}>
-      <label
-        htmlFor={id}
-        style={{ fontWeight: 600, fontSize: disabled ? 16 : 14 }}
-      >
-        {label}
-        {disabled && ": "}
-      </label>
-      <Input id={id} disabled={disabled} {...rest} />
-    </div>
-  );
-}
-
-const disabledStyles = {
-  border: "none",
-  background: "none"
-};
-
-const inputStyles = {
-  marginTop: 4,
-  padding: 8,
-  border: "1px solid hsl(220, 10%, 90%)",
-  borderRadius: 4
-};
-
-function Input({ disabled, ...rest }) {
-  return (
-    <input
-      style={disabled ? disabledStyles : inputStyles}
-      disabled={disabled}
-      {...rest}
-    />
   );
 }
 
